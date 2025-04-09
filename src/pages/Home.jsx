111f31f57240188492487991e5c71b0e0995eabd
Rodrigo Freitas <40476225+rodrigofreitasit@users.noreply.github.com>
2025-04-09 13:19:09 -0300

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,12 @@
+import CDBCalculator from '../components/CDBCalculator';
 
+function Home() {
+  return (
+    <div>
+      <h1>Simulador de Investimentos</h1>
+      <CDBCalculator />
+    </div>
+  );
+}
+
+export default Home;
